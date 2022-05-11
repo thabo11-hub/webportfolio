ad8f063840dff9fc08e394de9cb2778b5133befd
thabo <thabo.sodi@dadevs.co.za>
2022-05-11 15:57:59 +0200

    fix loader

--- cv/src/app/app.module.ts
+++ cv/src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxUiLoaderModule } from 'ngx-ui-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SectionComponent } from './components/section/section/section/section.component';
-import { EducationComponent } from './components/section/section/education/education/education.component';
-
+import { EducationComponent } from './components/myeducation/education.component';
+import { SectionComponent } from './components/herosection/section.component';
+import { LoaderComponent } from './components/loader/loader.component';
 @NgModule({
   declarations: [
     AppComponent,
     SectionComponent,
-    EducationComponent
+    EducationComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxUiLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
